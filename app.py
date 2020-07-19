7e74bb0ead80b92d000fc765fc8bae4d0ffd5ec8
hosam <hosamelshazly92@gmail.com>
2020-07-19 19:44:36 +0200

    implement search on artists

--- app.py
+++ app.py
@@ -252,18 +252,10 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  term = request.form.get('search_term', '')
+  data = Artist.query.filter(Artist.name.ilike('%' + term + '%')).all()
+  count = Artist.query.filter(Artist.name.ilike('%' + term + '%')).count()
+  return render_template('pages/search_artists.html', data=data, count = count, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
@@ -507,7 +499,7 @@ if not app.debug:
 
 # Default port:
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
 
 # Or specify port manually:
 '''
