1d79d762551296ead2d2bb390d0e6a369652e522
hosam <hosamelshazly92@gmail.com>
2020-07-26 09:23:22 +0200

    edit heroku database url

--- config.py
+++ config.py
@@ -3,14 +3,16 @@ SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
+ENV = 'dev';
 
-# Connect to the database
-
-
-# TODO_DONE IMPLEMENT DATABASE URL
-# SQLALCHEMY_DATABASE_URI = 'postgresql://admin:0008@localhost:5432/fyyur'
-DATABASE_URL = 'postgresql://admin:0008@localhost:5432/fyyur'
+if ENV == 'dev':
+    # Enable debug mode.
+    DEBUG = True
+    # Connect to the database
+    # TODO_DONE IMPLEMENT DATABASE URL
+    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:0008@localhost:5432/fyyur'
+else:
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgres://pntdfomabqppgb:300f71e6521c3bf76a57e0c5a083b7baf41a014acc06186590ffcd018d215a3f@ec2-54-234-28-165.compute-1.amazonaws.com:5432/d8cjoh1e4c4gdg'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
