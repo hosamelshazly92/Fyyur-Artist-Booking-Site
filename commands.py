e01d3809bf88d7b24e35f86ecb1a9a01ec95729a
hosam <hosamelshazly92@gmail.com>
2020-07-26 09:52:24 +0200

    edit commands file

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 import click
 from flask.cli import with_appcontext
 
-from models import db, artist, venue, show
+from model import db, artist, venue, show
 
 @click.command(name='create_tables')
 @with_appcontext
